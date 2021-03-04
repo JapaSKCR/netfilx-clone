de7a3c997152a5ab9f497a54bc531754b20efb41
Juliano Silva <japa_silva696@hotmail.com>
2021-03-04 00:52:08 -0300

    Primeiro commit

---
+++ js/owl/main.js
@@ -0,0 +1,13 @@
+function trailer(){
+    var yourWindow = window.open();
+    yourWindow.opener = null;
+    yourWindow.location = "https://www.youtube.com/watch?v=RNwNA1M8A94";
+    
+}
+
+function info(){
+    var yourWindow = window.open();
+    yourWindow.opener = null;
+    yourWindow.location = "https://fma.fandom.com/pt-br/wiki/Fullmetal_Alchemist:_Brotherhood";
+    
+}
